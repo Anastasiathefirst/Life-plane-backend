df3c1edbf900e3716e78fc6fad5585797216d17e
Anastasia <anastasia@macbook-pro-anastasia-1.home>
2025-05-04 17:18:07 +0200

    fix: updated auth controller and env config

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -8,163 +8,168 @@ import Token from '~/models/tokenModel';
 import Role from '~/models/roleModel';
 
 export const signup = async (req, res) => {
-	const role = await Role.getRoleByName('User');
-	req.body.roles = [role.id];
-	const user = await User.createUser(req.body);
-	const tokens = await tokenService.generateAuthTokens(user);
-	await emailService.sendVerificationEmail(user.email, tokens.access.token);
-	return res.json({
-		success: true,
-		data: { user, tokens }
-	});
+  const role = await Role.getRoleByName('User');
+  req.body.roles = [role.id];
+
+  const user = await User.createUser(req.body);
+  const tokens = await tokenService.generateAuthTokens(user);
+
+  // Генерим токен ПРАВИЛЬНЫЙ — VERIFY_EMAIL
+  const verifyEmailToken = await tokenService.generateVerifyEmailToken(user);
+  await emailService.sendVerificationEmail(user.email, verifyEmailToken);
+
+  return res.json({
+    success: true,
+    data: { user, tokens }
+  });
 };
 
 export const signin = async (req, res) => {
-	const user = await User.getUserByUserName(req.body.userName);
-	if (!user || !(await user.isPasswordMatch(req.body.password))) {
-		throw new APIError('Incorrect user name or password', httpStatus.BAD_REQUEST);
-	}
-	if (!user.confirmed) {
-		throw new APIError('Please verify your email before logging in.', httpStatus.UNAUTHORIZED);
-	}
-	const tokens = await tokenService.generateAuthTokens(user);
-	return res.json({
-		success: true,
-		data: { user, tokens }
-	});
+  const user = await User.getUserByUserName(req.body.userName);
+  if (!user || !(await user.isPasswordMatch(req.body.password))) {
+    throw new APIError('Incorrect user name or password', httpStatus.BAD_REQUEST);
+  }
+  if (!user.confirmed) {
+    throw new APIError('Please verify your email before logging in.', httpStatus.UNAUTHORIZED);
+  }
+  const tokens = await tokenService.generateAuthTokens(user);
+  return res.json({
+    success: true,
+    data: { user, tokens }
+  });
 };
 
 export const current = async (req, res) => {
-	const user = await User.getUserById(req.user.id);
-	if (!user) {
-		throw new APIError('User not found', httpStatus.NOT_FOUND);
-	}
-	return res.json({
-		success: true,
-		data: {
-			firstName: user.firstName,
-			lastName: user.lastName,
-			userName: user.userName,
-			avatarUrl: user.avatarUrl
-		}
-	});
+  const user = await User.getUserById(req.user.id);
+  if (!user) {
+    throw new APIError('User not found', httpStatus.NOT_FOUND);
+  }
+  return res.json({
+    success: true,
+    data: {
+      firstName: user.firstName,
+      lastName: user.lastName,
+      userName: user.userName,
+      avatarUrl: user.avatarUrl
+    }
+  });
 };
 
 export const getMe = async (req, res) => {
-	const user = await User.getUserByIdWithRoles(req.user.id);
-	if (!user) {
-		throw new APIError('User not found', httpStatus.NOT_FOUND);
-	}
-	return res.json({
-		success: true,
-		data: user
-	});
+  const user = await User.getUserByIdWithRoles(req.user.id);
+  if (!user) {
+    throw new APIError('User not found', httpStatus.NOT_FOUND);
+  }
+  return res.json({
+    success: true,
+    data: user
+  });
 };
 
 export const updateMe = async (req, res) => {
-	const user = await User.updateUserById(req.user.id, req.body);
-	return res.json({
-		success: true,
-		data: user
-	});
+  const user = await User.updateUserById(req.user.id, req.body);
+  return res.json({
+    success: true,
+    data: user
+  });
 };
 
 export const signout = async (req, res) => {
-	await Token.revokeToken(req.body.refreshToken, config.TOKEN_TYPES.REFRESH);
-	return res.json({
-		success: true,
-		data: 'Signout success'
-	});
+  await Token.revokeToken(req.body.refreshToken, config.TOKEN_TYPES.REFRESH);
+  return res.json({
+    success: true,
+    data: 'Signout success'
+  });
 };
 
 export const refreshTokens = async (req, res) => {
-	try {
-		const refreshTokenDoc = await tokenService.verifyToken(req.body.refreshToken, config.TOKEN_TYPES.REFRESH);
-		const user = await User.getUserById(refreshTokenDoc.user);
-		if (!user) {
-			throw new Error();
-		}
-		await refreshTokenDoc.remove();
-		const tokens = await tokenService.generateAuthTokens(user);
-		return res.json({
-			success: true,
-			data: {
-				tokens
-			}
-		});
-	} catch (err) {
-		throw new APIError(err.message, httpStatus.UNAUTHORIZED);
-	}
+  try {
+    const refreshTokenDoc = await tokenService.verifyToken(req.body.refreshToken, config.TOKEN_TYPES.REFRESH);
+    const user = await User.getUserById(refreshTokenDoc.user);
+    if (!user) {
+      throw new Error();
+    }
+    await refreshTokenDoc.remove();
+    const tokens = await tokenService.generateAuthTokens(user);
+    return res.json({
+      success: true,
+      data: {
+        tokens
+      }
+    });
+  } catch (err) {
+    throw new APIError(err.message, httpStatus.UNAUTHORIZED);
+  }
 };
 
 export const sendVerificationEmail = async (req, res) => {
-	const user = await User.getUserByEmail(req.user.email);
-	if (user.confirmed) {
-		throw new APIError('Email verified', httpStatus.BAD_REQUEST);
-	}
-	const verifyEmailToken = await tokenService.generateVerifyEmailToken(req.user);
-	await emailService.sendVerificationEmail(req.user.email, verifyEmailToken);
-	return res.json({
-		success: true,
-		data: 'Send verification email success'
-	});
+  const user = await User.getUserByEmail(req.user.email);
+  if (user.confirmed) {
+    throw new APIError('Email already verified', httpStatus.BAD_REQUEST);
+  }
+  const verifyEmailToken = await tokenService.generateVerifyEmailToken(req.user);
+  await emailService.sendVerificationEmail(req.user.email, verifyEmailToken);
+  return res.json({
+    success: true,
+    data: 'Verification email sent'
+  });
 };
 
 export const verifyEmail = async (req, res) => {
-	try {
-		const verifyEmailTokenDoc = await tokenService.verifyToken(req.query.token, config.TOKEN_TYPES.VERIFY_EMAIL);
-		const user = await User.getUserById(verifyEmailTokenDoc.user);
-		if (!user) {
-			throw new Error();
-		}
-		await Token.deleteMany({ user: user.id, type: config.TOKEN_TYPES.VERIFY_EMAIL });
-		await User.updateUserById(user.id, { confirmed: true });
-		return res.json({
-			success: true,
-			data: 'Verify email success'
-		});
-	} catch (err) {
-		throw new APIError('Email verification failed', httpStatus.UNAUTHORIZED);
-	}
+  try {
+    const verifyEmailTokenDoc = await tokenService.verifyToken(req.query.token, config.TOKEN_TYPES.VERIFY_EMAIL);
+    const user = await User.getUserById(verifyEmailTokenDoc.user);
+    if (!user) {
+      throw new Error();
+    }
+    await Token.deleteMany({ user: user.id, type: config.TOKEN_TYPES.VERIFY_EMAIL });
+    await User.updateUserById(user.id, { confirmed: true });
+    return res.json({
+      success: true,
+      data: 'Email verified successfully'
+    });
+  } catch (err) {
+    throw new APIError('Email verification failed', httpStatus.UNAUTHORIZED);
+  }
 };
 
 export const forgotPassword = async (req, res) => {
-	const resetPasswordToken = await tokenService.generateResetPasswordToken(req.body.email);
-	await emailService.sendResetPasswordEmail(req.body.email, resetPasswordToken);
-	return res.json({
-		success: true,
-		data: 'Send forgot password email success'
-	});
+  const resetPasswordToken = await tokenService.generateResetPasswordToken(req.body.email);
+  await emailService.sendResetPasswordEmail(req.body.email, resetPasswordToken);
+  return res.json({
+    success: true,
+    data: 'Password reset email sent'
+  });
 };
 
 export const resetPassword = async (req, res) => {
-	try {
-		const resetPasswordTokenDoc = await tokenService.verifyToken(req.query.token, config.TOKEN_TYPES.RESET_PASSWORD);
-		const user = await User.getUserById(resetPasswordTokenDoc.user);
-		if (!user) {
-			throw new Error();
-		}
-		await Token.deleteMany({ user: user.id, type: config.TOKEN_TYPES.RESET_PASSWORD });
-		await User.updateUserById(user.id, { password: req.body.password });
-		return res.json({
-			success: true,
-			data: 'Reset password success'
-		});
-	} catch (err) {
-		throw new APIError('Password reset failed', httpStatus.UNAUTHORIZED);
-	}
+  try {
+    const resetPasswordTokenDoc = await tokenService.verifyToken(req.query.token, config.TOKEN_TYPES.RESET_PASSWORD);
+    const user = await User.getUserById(resetPasswordTokenDoc.user);
+    if (!user) {
+      throw new Error();
+    }
+    await Token.deleteMany({ user: user.id, type: config.TOKEN_TYPES.RESET_PASSWORD });
+    await User.updateUserById(user.id, { password: req.body.password });
+    return res.json({
+      success: true,
+      data: 'Password has been reset'
+    });
+  } catch (err) {
+    throw new APIError('Password reset failed', httpStatus.UNAUTHORIZED);
+  }
 };
 
 export default {
-	signup,
-	signin,
-	current,
-	getMe,
-	updateMe,
-	signout,
-	refreshTokens,
-	sendVerificationEmail,
-	verifyEmail,
-	forgotPassword,
-	resetPassword
+  signup,
+  signin,
+  current,
+  getMe,
+  updateMe,
+  signout,
+  refreshTokens,
+  sendVerificationEmail,
+  verifyEmail,
+  forgotPassword,
+  resetPassword
 };
