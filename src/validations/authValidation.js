73c138f6ad6685dc2d0926fbf3de5cfd194b3743
Anastasia <anastasia@MacBook-Pro-Anastasia.local>
2025-06-13 19:24:35 +0200

    fix: убраны обязательные поля userName, firstName, lastName

--- src/validations/authValidation.js
+++ src/validations/authValidation.js
@@ -43,9 +43,6 @@ export const resetPassword = {
 
 export const updateMe = {
   body: Joi.object().keys({
-    firstName: Joi.string().trim().min(2).max(66),
-    lastName: Joi.string().trim().min(2).max(66),
-    userName: Joi.string().alphanum().min(6).max(66),
     email: Joi.string().email(),
     password: Joi.string().trim().min(6).max(666),
     avatar: Joi.string().max(666),
